ec776f2639c581710c4b92c7377e6317eb0c743f
Totiruzi <totiruzi@yahoo.com>
2020-09-10 11:45:57 +0300

    Refactor  Searchbar component and App component to use hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../api/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
 const KEY = 'AIzaSyA5hvJ7N4rNSNnUaOO_S-HNKGYct6UOn4Q';
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
-  componentDidMount() {
-    this.onTermSubmit('dance');
-  }
-  onTermSubmit = async (term) => {
+
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  useEffect(() => {
+    onTermSubmit('dance');
+  }, []);
+
+  const onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
       params: {
         q: term,
@@ -21,34 +25,30 @@ class App extends React.Component {
       },
     });
 
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
   };
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  };
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />
-        <div className="ui stackable grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                videos={this.state.videos}
-                onVideoSelect={this.onVideoSelect}
-              />
-            </div>
+  // this helper method can be used in place of argumant provided for the onVideoSelect props on VideoDetail 
+  // -> onVideoSelect= {(video) => setSelectedVideo(video)}-< the current code is the equivilent
+  // const onVideoSelect = (video) => {
+  //   setSelectedVideo(video);
+  // };
+
+  return (
+    <div className="ui container">
+      <SearchBar onTermSubmit={onTermSubmit} />
+      <div className="ui stackable grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,29 +1,30 @@
-import React from 'react';
+import React, { useState} from 'react';
 
-class SearchBar extends React.Component {
-  state = { term: '' };
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
+const SearchBar = ({ onTermSubmit}) => {
+  const [term, setTerm] = useState('');
+
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
   };
-  onFormSubmit = (event) => {
+  const onFormSubmit = (event) => {
     event.preventDefault();
-    this.props.onTermSubmit(this.state.term);
+    onTermSubmit(term);
   }
-  render() {
-    return (
-      <div className="ui segment search-bar">
-        <form onSubmit={this.onFormSubmit} className="ui form">
-          <div className="field">
-            <label>Video Search</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
+
+  return (
+    <div className="ui segment search-bar">
+      <form onSubmit={onFormSubmit} className="ui form">
+        <div className="field">
+          <label>Video Search</label>
+          <input
+            type="text"
+            value={term}
+            onChange={onInputChange}
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
+
 export default SearchBar;
